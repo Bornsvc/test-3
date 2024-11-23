5e53d0452d3e9f016703d07760794c9d3fa923e2
Born <sayavongsouvannachack@gmail.com>
2024-11-23 17:47:21 +0700

    update

--- Javascript/specialoffer.js
+++ Javascript/specialoffer.js
@@ -1,25 +1,31 @@
+
+
 let currentIndex = 0;
         const cards = document.querySelectorAll('.slider-container .card');
         const totalCards = cards.length;
         const cardsToShow = 4;
-
+        
+        function slideRight() {
+            if(currentIndex < totalCards - 4){
+                currentIndex++;
+                updateSlider();
+            }else {
+                currentIndex = 0;
+            }
+            
+        }
+        
         function slideLeft() {
             if (currentIndex > 0) {
                 currentIndex--;
                 updateSlider();
+            }else {
+                currentIndex = totalCards - 3
             }
         }
 
-        function slideRight() {
-            if (currentIndex < totalCards - cardsToShow) {
-                currentIndex++;
-                updateSlider();
-            }else {
-                currentIndex = 0
-            }
-        }
 
         function updateSlider() {
             const cardWidth = cards[0].clientWidth;
-            document.querySelector('.slider-container').style.transform = `translateX(-${currentIndex * (cardWidth + 20)}px)`;
+            document.querySelector('.slider-container').style.transform = `translateX(-${currentIndex * (cardWidth + 20)}px`;
         }
--- Javascript/team.js
+++ Javascript/team.js
@@ -1,11 +1,11 @@
-// let slider = document.querySelector(".container-swiper");
-// let nextBtn = document.getElementById("next");
-// let prevBtn = document.getElementById("prev");
+let slider = document.querySelector(".container-swiper");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
 
 
-// nextBtn.onclick = () => {
-//     slider.append(slider.querySelector('.card:first-child'));
-// }
-// prevBtn.onclick = () => {
-//     slider.prepend(slider.querySelector('.card:last-child'))
-// }
+nextBtn.onclick = () => {
+    slider.append(slider.querySelector('.card:first-child'));
+}
+prevBtn.onclick = () => {
+    slider.prepend(slider.querySelector('.card:last-child'))
+}
